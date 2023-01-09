7b5d043f91da367846c8447d38192ee9366da146
NguyenVinhThien <20110133@student.hcmute.edu.vn>
2023-01-10 03:36:09 +0700

    chinh sua path va them frontend

--- src/main/java/Controller/TrangChu.java
+++ src/main/java/Controller/TrangChu.java
@@ -4,6 +4,7 @@ import DAO.DAOAdmin;
 import Model.ArticleHasCategories;
 import Model.Categories;
 import Model.Tags;
+import Uti.ServletUtils;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -14,36 +15,57 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet(name = "TrangChu", value = "/TrangChu")
+@WebServlet(name = "TrangChu", urlPatterns = "/TrangChu/*")
 public class TrangChu extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
-        DAOAdmin d= new DAOAdmin();
-        List<Categories> listMainCat= d.getAllMainCategories();
-        request.setAttribute("listCat", listMainCat);
+        String path= request.getPathInfo();
+        if(path == null || path.equals("/"))
+        {
+            path= "/Index";
+        }
+        switch (path) {
+            case "/Index":
+                DAOAdmin d = new DAOAdmin();
 
-        List<Categories> listSubCat= d.getSubCategories();
-        request.setAttribute("listSubCat", listSubCat);
+                List<Categories> listMainCat = d.getAllMainCategories();
+                request.setAttribute("listCat", listMainCat);
 
-        List<Tags> listTags= d.getAllTag();
-        request.setAttribute("listTags", listTags);
+                List<Categories> listSubCat = d.getSubCategories();
+                request.setAttribute("listSubCat", listSubCat);
 
-//        Map<Articles,String> listTopHotArt= d.getTopHotArticle();
-        List<ArticleHasCategories> listTopHotArt= d.getTopHotArticle();
-        request.setAttribute("listTopHotArt", listTopHotArt);
+                List<Tags> listTags = d.getAllTag();
+                request.setAttribute("listTags", listTags);
 
-        List<ArticleHasCategories> listArtByView= d.getArticleByView();
-        request.setAttribute("listArtByView", listArtByView);
+                List<ArticleHasCategories> listTopHotArt = d.getTopHotArticle();
+                request.setAttribute("listTopHotArt", listTopHotArt);
 
-        List<ArticleHasCategories> listNewArt= d.getNewArticle();
-        request.setAttribute("listNewArt", listNewArt);
+                List<ArticleHasCategories> listArtByView = d.getArticleByView();
+                request.setAttribute("listArtByView", listArtByView);
 
-        List<ArticleHasCategories> listTopByCat= d.getTopByCat();
-        request.setAttribute("listTopByCat", listTopByCat);
+                List<ArticleHasCategories> listNewArt = d.getNewArticle();
+                request.setAttribute("listNewArt", listNewArt);
 
-        RequestDispatcher rd= request.getRequestDispatcher("TrangChu.jsp");
-        rd.forward(request, response);
+                List<ArticleHasCategories> listTopByCat = d.getTopByCat();
+                request.setAttribute("listTopByCat", listTopByCat);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/TrangChu.jsp");
+                rd.forward(request, response);
+                break;
+
+            case "/ShowArticleByCat":
+
+                ServletUtils.forward("/ShowArticleByCat.jsp", request, response);
+                break;
+
+            default:
+                ServletUtils.forward("/404.jsp", request, response);
+                break;
+
+        }
+
+
     }
 
     @Override
